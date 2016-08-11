ca1d7bf4b38fbc16124464b3d25fea11fa866d1d
kaku <guoxiaojun8804@126.com>
2016-08-11 15:50:21 +0800

    新增文件下载功能

--- app/src/main/java/activity/app/gxj/com/okhttpdemo/MainActivity.java
+++ app/src/main/java/activity/app/gxj/com/okhttpdemo/MainActivity.java
@@ -1,6 +1,7 @@
 package activity.app.gxj.com.okhttpdemo;
 
 import android.content.Intent;
+import android.os.Environment;
 import android.os.Handler;
 import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
@@ -11,7 +12,10 @@ import android.widget.Button;
 import android.widget.TextView;
 
 
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.net.CookieManager;
 import java.net.CookiePolicy;
 import java.util.concurrent.TimeUnit;
@@ -50,7 +54,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
 
         result = (TextView) findViewById(R.id.result);
         cache = (Button) findViewById(R.id.cache);
@@ -125,8 +128,37 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    public void postReqest(View view){
+    public void downLoadFile(View view){
+        String url = "http://p0.ifengimg.com/a/2016_33/abe707743d66cdc_size27_w570_h692.jpg";
+        Request request = new Request.Builder().url(url).build();
+        OkHttpClient client = new OkHttpClient();
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
 
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+
+                //把请求成功的response转为字节流
+                InputStream inputStream = response.body().byteStream();
+                //在这里用到了文件输出流
+                FileOutputStream fileOutputStream =
+                        new FileOutputStream(new File(
+                                Environment.getExternalStorageDirectory(),"/nzt.jpg"));
+                //定义一个字节数组
+                byte[] buffer = new byte[2048];
+                int len = 0;
+                while ((len = inputStream.read(buffer)) != -1) {
+                    //写出到文件
+                    fileOutputStream.write(buffer, 0, len);
+                }
+                //关闭输出流
+                fileOutputStream.flush();
+                Log.d("wuyinlei", "文件下载成功...");
+            }
+        });
     }
 
 
